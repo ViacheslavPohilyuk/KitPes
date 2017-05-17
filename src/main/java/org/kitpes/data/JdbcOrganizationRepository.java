38b6c8dc41dae4c70cb1ddad257a1a584d11148c
Serhii Chornoivan <serhiichornoivan@gmail.com>
2017-05-17 17:47:06 +0300

    add db operations

--- src/main/java/org/kitpes/data/JdbcOrganizationRepository.java
+++ src/main/java/org/kitpes/data/JdbcOrganizationRepository.java
@@ -1,10 +1,15 @@
 package org.kitpes.data;
 
 import org.kitpes.model.Organization;
+import org.kitpes.model.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -21,7 +26,10 @@ public class JdbcOrganizationRepository implements OrganizationRepository {
 
     @Override
     public List<Organization> readAll() {
-        return null;
+        return jdbc.query(
+                "SELECT * FROM organization",
+                new Orga
+        );
     }
 
     @Override
@@ -42,5 +50,20 @@ public class JdbcOrganizationRepository implements OrganizationRepository {
     @Override
     public long save(Organization organization) {
         return 0;
+    }
+
+    private static class OrganizationRowMapper implements RowMapper<Organization>, Serializable {
+        OrganizationRowMapper() {
+        }
+        public Organization mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Organization(rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("animal"),
+                    rs.getInt("age"),
+                    rs.getString("sex"),
+                    rs.getString("description"),
+                    rs.getString("status"),
+                    rs.getString("organization"));
+        }
     }
 }
